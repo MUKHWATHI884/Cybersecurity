ba538de21762364dfa4dd8462b2a9f1c0ab84718
Student <Student@LabVM2047644>
2026-08-20 14:26:11 +0200

    Add project files.

---
+++ Cybersecurity/Logo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cybersecurity
+{
+    public void ShowLogo()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.WriteLine("████████╗██╗  ██╗██╗ █████╗  ██████╗  ██████╗ \r\n" +
+            "╚══██╔══╝██║  ██║██║██╔══██╗██╔════╝ ██╔═══██╗\r\n" +
+            "   ██║   ███████║██║███████║██║  ███╗██║   ██║\r\n" +
+            "   ██║   ██╔══██║██║██╔══██║██║   ██║██║   ██║\r\n" +
+            "   ██║   ██║  ██║██║██║  ██║╚██████╔╝╚██████╔╝\r\n" +
+            "   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ \r\n                                              ");
+
+    }
+}
---
+++ Cybersecurity/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using Cybersecurity;
+
+
+namespace Cybersecurity
+{
+
+    public class Programming
+    {
+        static void Main(string[] args)
+        {
+            Logo logo = new Logo();
+            logo.ShowLogo();
+
+            SoundPlayer player = new System.Media.SoundPlayer();
+            player.Play();
+
+            Responspe response = new Response();
+            response.StartChat();
+        }
+    }
+
+}
